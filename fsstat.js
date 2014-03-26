2b81eae2b82a8317c6cac29bb6b3eec90f91a778
user <user@domain>
2014-03-26 16:15:26 +0100

    s

--- fsstat.js
+++ fsstat.js
@@ -1,8 +1,1 @@
-var fs = require('fs');
-
-var stat = fs.statSync('data/user.db');
-<<<<<<< HEAD
-console .log(stat);
-=======
-console.log(stat); 
->>>>>>> 777526ec112658ecb8715b65be62910a0400a3b4
+window.addEventListener() 
